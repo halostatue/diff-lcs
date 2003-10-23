8bd6c5052da519d9619f609f9a2c0477e0e7a139
(no author) <(no author)>
2003-10-23 00:49:11 +0000

    This commit was manufactured by cvs2svn to create tag 'version_0_6_2'.

---
+++ tags/version_0_6_2/ruwiki/lib/ruwiki/page.rb
@@ -0,0 +1,131 @@
+#--
+# Ruwiki
+#   Copyright © 2002 - 2003, Digikata and HaloStatue
+#   Alan Chen (alan@digikata.com)
+#   Austin Ziegler (ruwiki@halostatue.ca)
+#
+# Licensed under the same terms as Ruby.
+#
+# $Id$
+#++
+class Ruwiki
+    # A generic page for Ruwiki.
+  class Page
+      # The page ID.
+    attr_accessor :page_id
+      # The current version of the page.
+    attr_accessor :version
+      # The previous version of the page.
+    attr_accessor :old_version
+      # The page topic (the name of the page).
+    attr_accessor :topic
+      # The project of the page.
+    attr_accessor :project
+      # Unformatted page text.
+    attr_reader   :content
+      # Formatted page text.
+    attr_accessor :formatted
+
+      # The IP address of the person who made the last change.
+    def change_ip
+      %Q(#{@remote_host} #{@remote_addr})
+    end
+
+      # The ID, if present, of the person who made the last change. Not yet
+      # implemented.
+    def change_id
+      nil
+    end
+
+      # Creates a Ruwiki page.
+    def initialize(init = {})
+      @markup       = init[:markup]
+      @script       = init[:script]
+
+      @remote_host  = init[:remote_host]
+      @remote_addr  = init[:remote_addr]
+
+      @project      = init[:project] || "Default"
+      @topic        = init[:topic]   || "NewTopic"
+      @content      = init[:content] || ""
+      @page_id      = init[:page_id] || 0
+      @version      = init[:version] || 0
+      @old_version  = @version - 1
+
+      if init.has_key?(:rawtext)
+        @rawtext    = init[:rawtext].dup
+        @content    = parse_header(@rawtext.dup)
+        @formatted  = parse_content(@content, @project)
+      elsif not @content.empty?
+        @formatted  = parse_content(@content.dup, @project)
+      else
+        @formatted  = ""
+      end
+      @content.gsub!(/\r/, "")
+    end
+
+      # The content of the page.
+    def content=(content)
+      @content    = content.gsub(/\r/, "")
+      @formatted  = parse_content(content, @project)
+    end
+
+      # Output raw header and raw page context for saving.
+    def rawtext
+      return <<-EOS
+id: #{@page_id}
+topic: #{@topic}
+version: #{@version}
+#EHDR
+#{@content}
+      EOS
+    end
+
+      # Outputs the HTML version of the page.
+    def to_html
+      @formatted
+    end
+
+  private
+    HEADER_RE     = /^([a-z]+)\s*:\s*(.*)$/
+    HEADER_END_RE = /^#EHDR$/
+
+      # Parse the header.
+    def parse_header(rawtext)
+      rawbuf = rawtext.split("\n")
+
+      loop do
+        break if rawbuf.nil? or rawbuf.empty?
+
+        if rawbuf[0] =~ HEADER_END_RE
+          rawbuf.shift
+          break
+        end
+
+        match = HEADER_RE.match(rawbuf[0])
+
+        if match
+          case match[1].intern
+          when :id
+            @page_id = match[2].to_i
+          when :topic
+            @topic = match[2]
+          when :version
+            @version      = match[2].to_i
+            @old_version  = @version - 1
+          end
+          rawbuf.shift
+        end
+      end
+
+      rawbuf.join("\n")
+    end
+
+      # Parse the content.
+    def parse_content(content, project)
+      parsed = @markup.parse(content, project)
+
+      parsed
+    end
+  end
+end
---
+++ tags/version_0_6_2/ruwiki/lib/ruwiki/wiki/tokens/00default.rb
@@ -0,0 +1,174 @@
+#--
+# Ruwiki
+#   Copyright © 2002 - 2003, Digikata and HaloStatue
+#   Alan Chen (alan@digikata.com)
+#   Austin Ziegler (ruwiki@halostatue.ca)
+#
+# Licensed under the same terms as Ruby.
+#
+# $Id$
+#++
+
+class Ruwiki
+  class Wiki
+      # The Paragraph Token class changes blank lines to <p> tags. This class,
+      # under the current implementation, should be *first* in the Token list
+      # after Token.
+    class Paragraph < Ruwiki::Wiki::Token
+        # This Token is #rank 0, because it should be first in the Token list.
+      def self.rank
+        0
+      end
+
+        # Matches blank lines. %r{^\s*$}
+      def self.regexp
+        %r{(^\s*$)}
+      end
+
+        # Replaces with "<p>"
+      def replace
+        "<p>"
+      end
+
+        # Ensures that <p> won't be surrounded by <br> tags.
+      def post_replace(content)
+        content.gsub!(%r{\n$}, '</p>')
+        content.gsub!(%r{(\n|<br ?/?>)?<p>(\n|<br ?/?>)?}, '<p>')
+        content.gsub!(%r{<p>}, '</p><p>')
+        content.gsub!(%r{</p>(</p>)+}, '</p>')
+        content.gsub!(%r{<body></p>}, '<body>')
+        content.gsub!(%r{</body>}, '</p></body>')
+        content.gsub!(%r{<p></p>}, '')
+        content.gsub!(%r{(</h\d>)</p>}, '\1')
+        content.gsub!(%r{^</p>}, '')
+        content
+    end
+  end
+
+      # The Code Token class converts indented text to "unformatted" (<pre>)
+      # text. This class should be *second* in the Token list.
+    class Code < Ruwiki::Wiki::Token
+        # This Token is #rank 1, because it should be second in the Token list.
+      def self.rank
+        1
+      end
+
+        # Matches indented text. %r{^(\s+\S.*)$}
+      def self.regexp
+        %r{^(\s+\S.*)$}
+      end
+
+        # Replaces the text to <pre>content</pre>.
+      def replace
+        content = @match[1].gsub(/&/) { "&amp;" }.gsub(/</) { "&lt;" }.gsub(/>/) { "&gt;" }
+        %Q{<pre>#{content}</pre>}
+      end
+
+        # Converts cases of %r{</pre>(\n|<br ?/?>)<pre>} to \1.
+      def post_replace(content)
+        content.gsub!(%r{</pre>(\n|<br ?/?>)?<pre>}, '\1')
+        content.gsub!(%r{<p><pre>}, '<pre>')
+        content.gsub!(%r{</pre></p>}, '</pre>')
+        content
+      end
+    end
+
+    RE_URI_SCHEME = %r{[\w.]+?:}
+    RE_URI_PATH   = %r{[^\s<>\]]}
+    RE_URI_TEXT   = %r{[^\]]*}
+    RE_IMAGE      = /(jpg|jpeg|png|gif)$/
+
+      # Converts URLs in the form of [url] to numbered links.
+    class NumberedLinks < Ruwiki::Wiki::Token
+      class << self
+        attr_accessor :count
+      end
+
+      def self.rank
+        2
+      end
+
+      def self.regexp
+        %r{\[(#{RE_URI_SCHEME}(?:#{RE_URI_PATH})*?)\]}
+      end
+
+      def replace
+        extlink = @match.captures[0]
+
+        NumberedLinks.count ||= 0
+        NumberedLinks.count += 1
+        name = "[#{NumberedLinks.count}]"
+
+        if extlink =~ RE_IMAGE
+          %Q{<img src="#{extlink}" title="#{name}" alt="#{name}" />}
+        else
+          %Q{<a class="rw_extlink" href="#{extlink}">#{name}</a>}
+        end
+      end
+    end
+
+      # Converts URLs in the form of [url name] to named links.
+    class NamedLinks < Ruwiki::Wiki::Token
+      def self.rank
+        3
+      end
+
+      def self.regexp
+        %r{\[(#{RE_URI_SCHEME}(?:#{RE_URI_PATH})*?)\s+(#{RE_URI_TEXT})\]}
+      end
+
+      def replace
+        extlink = @match.captures[0]
+        name    = @match.captures[1]
+
+        if extlink =~ RE_IMAGE
+          %Q{<img src="#{extlink}" title="#{name}" alt="#{name}" />}
+        else
+          %Q{<a class="rw_extlink" href="#{extlink}">#{name}</a>}
+        end
+      end
+    end
+
+      # Converts URLs to links where the "name" of the link is the URL itself.
+    class ExternalLinks < Ruwiki::Wiki::Token
+      def self.rank
+        504
+      end
+
+      def self.regexp
+        %r{\b(#{RE_URI_SCHEME}#{RE_URI_PATH}+)}
+      end
+
+      def replace
+        extlink = @match[1]
+
+        if extlink =~ RE_IMAGE
+          %Q{<img src="#{extlink}" title="Image at: #{extlink}" alt="Image at: #{extlink}" />}
+        else
+          %Q{<a class="rw_extlink" href="#{extlink}">#{extlink}</a>}
+        end
+      end
+    end
+
+      # Creates a horizontal rule.
+    class HRule < Ruwiki::Wiki::Token
+      def self.regexp
+        %r|^\\?-{4,}|
+      end
+
+      def replace
+        "<hr />"
+      end
+
+      def restore
+        @match[0][1 .. -1]
+      end
+
+      def post_replace(content)
+        content.gsub!(%r{<hr ?/?>\n<br ?/?>}, "<hr />")
+        content.gsub!(%r{(\n|<br ?/?>)?<hr>(\n|<br ?/?>)?}, "<hr />")
+        content
+      end
+    end
+  end
+end
