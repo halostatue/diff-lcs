89906cff2d7a54925422230728684d2fa300e5fc
Akinori MUSHA <knu@idaemons.org>
2019-01-24 17:13:24 +0900

    Add #to_ary to Diff::LCS::Change and Diff::LCS::ContextChange
    It would be quite handy if you could write as follows:
    
    ```ruby
    Diff::LCS.sdiff(a, b).each do |action, (old_position, old_element), (new_position, new_element)|
      case action
      when '!'
        # replace
      when '-'
        # delete
      when '+'
        # insert
      end
    end
    ```

--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -62,4 +62,28 @@ describe Diff::LCS::Change do
     it { should_not be_finished_a }
     it { should     be_finished_b }
   end
+
+  describe "as array" do
+    it "should be converted" do
+      action, position, element = described_class.new('!', 0, 'element')
+      expect(action).to eq '!'
+      expect(position).to eq 0
+      expect(element).to eq 'element'
+    end
+  end
+end
+
+describe Diff::LCS::ContextChange do
+  describe "as array" do
+    it "should be converted" do
+      action, (old_position, old_element), (new_position, new_element) =
+        described_class.new('!', 1, 'old_element', 2, 'new_element')
+
+      expect(action).to eq '!'
+      expect(old_position).to eq 1
+      expect(old_element).to eq 'old_element'
+      expect(new_position).to eq 2
+      expect(new_element).to eq 'new_element'
+    end
+  end
 end
