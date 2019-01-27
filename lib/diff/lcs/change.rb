07ed577eba341f0ff0d7eebf4c1c2cc23083cba7
Austin Ziegler <austin@zieglers.ca>
2019-01-27 00:03:42 -0500

    Revert PR#47 "Add #to_ary to Diff::LCS::Change…"
    This reverts commits 01e0cae and 3b4d2be because they introduce fatally
    breaking changes in the tests that were hidden by broken CI.
    
    * 01e0cae: Add #to_ary to Diff::LCS::Change and Diff::LCS::ContextChange
      [Akinori MUSHA]
    * 3b4d2be: Mention in rdoc that Diff::LCS::ContextChange can be converted to an
      array [Akinori MUSHA]

--- lib/diff/lcs/change.rb
+++ lib/diff/lcs/change.rb
@@ -41,8 +41,6 @@ class Diff::LCS::Change
     [ @action, @position, @element ]
   end
 
-  alias to_ary to_a
-
   def self.from_a(arr)
     arr = arr.flatten(1)
     case arr.size
@@ -133,8 +131,6 @@ class Diff::LCS::ContextChange < Diff::LCS::Change
       [ @new_position, @new_element ]
     ]
   end
-
-  alias to_ary to_a
 
   def inspect(*args)
     to_a.inspect
