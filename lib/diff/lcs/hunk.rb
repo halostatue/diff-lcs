44ebd59e3272e67ed283f85c75522e1f5cfac486
Austin Ziegler <austin@zieglers.ca>
2025-07-27 14:52:14 -0400

    feat: Modernize LCS Hunk processing
    Optimized hunk processing by caching encoding-related strings and
    removing unnecessary encoding detection conditionals.
    
    Signed-off-by: Austin Ziegler <austin@zieglers.ca>

--- lib/diff/lcs/hunk.rb
+++ lib/diff/lcs/hunk.rb
@@ -2,16 +2,15 @@
 
 require "diff/lcs/block"
 
-# A Hunk is a group of Blocks which overlap because of the context surrounding
-# each block. (So if we're not using context, every hunk will contain one
-# block.) Used in the diff program (bin/ldiff).
+# A Hunk is a group of Blocks which overlap because of the context surrounding each block.
+# (So if we're not using context, every hunk will contain one block.) Used in the diff
+# program (bin/ldiff).
 class Diff::LCS::Hunk
   OLD_DIFF_OP_ACTION = {"+" => "a", "-" => "d", "!" => "c"}.freeze # :nodoc:
+  private_constant :OLD_DIFF_OP_ACTION
 
-  private_constant :OLD_DIFF_OP_ACTION if respond_to?(:private_constant)
-
-  # Create a hunk using references to both the old and new data, as well as the
-  # piece of data.
+  # Create a hunk using references to both the old and new data, as well as the piece of
+  # data.
   def initialize(data_old, data_new, piece, flag_context, file_length_difference)
     # At first, a hunk will have just one Block in it
     @blocks = [Diff::LCS::Block.from_chunk(piece)]
@@ -20,9 +19,9 @@ class Diff::LCS::Hunk
       fail "Cannot build a hunk from #{piece.inspect}; has no add or remove actions"
     end
 
-    if String.method_defined?(:encoding)
-      @preferred_data_encoding = data_old.fetch(0) { data_new.fetch(0) { "" } }.encoding
-    end
+    @preferred_data_encoding = data_old.fetch(0) { data_new.fetch(0) { "" } }.encoding
+    @newline = "\n".encode(@preferred_data_encoding)
+    @missing_newline = "\\ No newline at end of file".encode(@preferred_data_encoding)
 
     @data_old = data_old
     @data_new = data_new
@@ -34,10 +33,9 @@ class Diff::LCS::Hunk
     @file_length_difference = after # The caller must get this manually
     @max_diff_size = @blocks.map { |e| e.diff_size.abs }.max
 
-    # Save the start & end of each array. If the array doesn't exist (e.g.,
-    # we're only adding items in this block), then figure out the line number
-    # based on the line number of the other file and the current difference in
-    # file lengths.
+    # Save the start and end of each array. If the array doesn't exist (e.g., we're only
+    # adding items in this block), then figure out the line number based on the line
+    # number of the other file and the current difference in file lengths.
     if @blocks[0].remove.empty?
       a1 = a2 = nil
     else
@@ -65,11 +63,13 @@ class Diff::LCS::Hunk
   attr_reader :end_old, :end_new
   attr_reader :file_length_difference
 
-  # Change the "start" and "end" fields to note that context should be added
-  # to this hunk.
-  attr_accessor :flag_context
-  undef :flag_context=
-  def flag_context=(context) # :nodoc: # standard:disable Lint/DuplicateMethods
+  ##
+  # Change the "start" and "end" fields to note that context should be added to this hunk.
+  # :attr_accessor: :flag_context
+  attr_reader :flag_context
+
+  ##
+  def flag_context=(context) # :nodoc:
     return if context.nil? || context.zero?
 
     add_start = (context > @start_old) ? @start_old : context
@@ -90,9 +90,8 @@ class Diff::LCS::Hunk
     @end_new += add_end
   end
 
-  # Merges this hunk and the provided hunk together if they overlap. Returns
-  # a truthy value so that if there is no overlap, you can know the merge
-  # was skipped.
+  # Merges this hunk and the provided hunk together if they overlap. Returns a truthy
+  # value so that if there is no overlap, you can know the merge was skipped.
   def merge(hunk)
     return unless overlaps?(hunk)
 
@@ -102,12 +101,11 @@ class Diff::LCS::Hunk
   end
   alias_method :unshift, :merge
 
-  # Determines whether there is an overlap between this hunk and the
-  # provided hunk. This will be true if the difference between the two hunks
-  # start or end positions is within one position of each other.
+  # Determines whether there is an overlap between this hunk and the provided hunk. This
+  # will be true if the difference between the two hunks start or end positions is within
+  # one position of each other.
   def overlaps?(hunk)
-    hunk and (((@start_old - hunk.end_old) <= 1) or
-              ((@start_new - hunk.end_new) <= 1))
+    hunk && (((@start_old - hunk.end_old) <= 1) || ((@start_new - hunk.end_new) <= 1))
   end
 
   # Returns a diff string based on a format.
@@ -124,10 +122,15 @@ class Diff::LCS::Hunk
     end
   end
 
-  # Note that an old diff can't have any context. Therefore, we know that
-  # there's only one block in the hunk.
+  private
+
+  # Note that an old diff can't have any context. Therefore, we know that there's only one
+  # block in the hunk.
   def old_diff(last = false)
     warn "Expecting only one block in an old diff hunk!" if @blocks.size > 1
+
+    del, ins, sep, _ = ["< ", "> ", "---\n", "\\ No newline at end of file\n"]
+      .map { _1.encode(@preferred_data_encoding) }
 
     block = @blocks[0]
 
@@ -136,47 +139,45 @@ class Diff::LCS::Hunk
       new_missing_newline = !@new_empty && missing_last_newline?(@data_new)
     end
 
-    # Calculate item number range. Old diff range is just like a context
-    # diff range, except the ranges are on one line with the action between
-    # them.
-    s = encode("#{context_range(:old, ",")}#{OLD_DIFF_OP_ACTION[block.op]}#{context_range(:new, ",")}\n")
-    # If removing anything, just print out all the remove lines in the hunk
-    # which is just all the remove lines in the block.
+    # Calculate item number range. Old diff range is just like a context diff range,
+    # except the ranges are on one line with the action between them.
+    s = "#{context_range(:old, ",")}#{OLD_DIFF_OP_ACTION[block.op]}#{context_range(:new, ",")}\n"
+      .encode(@preferred_data_encoding)
+    # If removing anything, just print out all the remove lines in the hunk which is just
+    # all the remove lines in the block.
     unless block.remove.empty?
-      @data_old[@start_old..@end_old].each { |e| s << encode("< ") + e.chomp + encode("\n") }
+      @data_old[@start_old..@end_old].each { |e| s << del + e.chomp + @newline }
     end
 
-    s << encode("\\ No newline at end of file\n") if old_missing_newline && !new_missing_newline
-    s << encode("---\n") if block.op == "!"
+    s << @missing_newline << @newline if old_missing_newline && !new_missing_newline
+    s << sep if block.op == "!"
 
     unless block.insert.empty?
-      @data_new[@start_new..@end_new].each { |e| s << encode("> ") + e.chomp + encode("\n") }
+      @data_new[@start_new..@end_new].each { |e| s << ins + e.chomp + @newline }
     end
 
-    s << encode("\\ No newline at end of file\n") if new_missing_newline && !old_missing_newline
+    s << @missing_newline << @newline if new_missing_newline && !old_missing_newline
 
     s
   end
-  private :old_diff
 
   def unified_diff(last = false)
     # Calculate item number range.
-    s = encode("@@ -#{unified_range(:old)} +#{unified_range(:new)} @@\n")
+    s = "@@ -#{unified_range(:old)} +#{unified_range(:new)} @@\n"
+      .encode(@preferred_data_encoding)
 
-    # Outlist starts containing the hunk of the old file. Removing an item
-    # just means putting a '-' in front of it. Inserting an item requires
-    # getting it from the new file and splicing it in. We splice in
-    # +num_added+ items. Remove blocks use +num_added+ because splicing
-    # changed the length of outlist.
+    # `outlist` starts containing the hunk of the old file. Removing an item just means
+    # putting a '-' in front of it. Inserting an item requires getting it from the new
+    # file and splicing it in. We splice in `num_added` items. Remove blocks use
+    # `num_added` because splicing changed the length of outlist.
     #
-    # We remove +num_removed+ items. Insert blocks use +num_removed+
-    # because their item numbers -- corresponding to positions in the NEW
-    # file -- don't take removed items into account.
+    # We remove `num_removed` items. Insert blocks use `num_removed` because their item
+    # numbers -- corresponding to positions in the NEW file -- don't take removed items
+    # into account.
     lo, hi, num_added, num_removed = @start_old, @end_old, 0, 0
 
-    # standard:disable Performance/UnfreezeString
-    outlist = @data_old[lo..hi].map { |e| String.new("#{encode(" ")}#{e.chomp}") }
-    # standard:enable Performance/UnfreezeString
+    space = " ".encode(@preferred_data_encoding)
+    outlist = @data_old[lo..hi].map { |e| "#{space}#{e.chomp}" }
 
     last_block = blocks[-1]
 
@@ -187,97 +188,92 @@ class Diff::LCS::Hunk
 
     @blocks.each do |block|
       block.remove.each do |item|
-        op = item.action.to_s # -
         offset = item.position - lo + num_added
-        outlist[offset][0, 1] = encode(op)
+        outlist[offset][0, 1] = item.action.to_s.encode(@preferred_data_encoding) # -
         num_removed += 1
       end
 
       if last && block == last_block && old_missing_newline && !new_missing_newline
-        outlist << encode('\\ No newline at end of file')
+        outlist << @missing_newline
         num_removed += 1
       end
 
       block.insert.each do |item|
-        op = item.action.to_s # +
+        op = item.action.to_s.encode(@preferred_data_encoding) # +
         offset = item.position - @start_new + num_removed
-        outlist[offset, 0] = encode(op) + @data_new[item.position].chomp
+        outlist[offset, 0] = op + @data_new[item.position].chomp
         num_added += 1
       end
     end
 
-    outlist << encode('\\ No newline at end of file') if last && new_missing_newline
+    outlist << @missing_newline if last && new_missing_newline
 
-    s << outlist.join(encode("\n"))
+    s << outlist.join("\n".encode(@preferred_data_encoding))
 
     s
   end
-  private :unified_diff
 
   def context_diff(last = false)
-    s = encode("***************\n")
-    s << encode("*** #{context_range(:old, ",")} ****\n")
+    s = "***************\n".encode(@preferred_data_encoding)
+    s << "*** #{context_range(:old, ",")} ****\n".encode(@preferred_data_encoding)
     r = context_range(:new, ",")
+
+    spaces = "  ".encode(@preferred_data_encoding)
 
     if last
       old_missing_newline = missing_last_newline?(@data_old)
       new_missing_newline = missing_last_newline?(@data_new)
     end
 
-    # Print out file 1 part for each block in context diff format if there
-    # are any blocks that remove items
+    # Print out file 1 part for each block in context diff format if there are any blocks
+    # that remove items
     lo, hi = @start_old, @end_old
     removes = @blocks.reject { |e| e.remove.empty? }
 
     unless removes.empty?
-      # standard:disable Performance/UnfreezeString
-      outlist = @data_old[lo..hi].map { |e| String.new("#{encode("  ")}#{e.chomp}") }
-      # standard:enable Performance/UnfreezeString
+      outlist = @data_old[lo..hi].map { |e| "#{spaces}#{e.chomp}" }
 
       last_block = removes[-1]
 
       removes.each do |block|
         block.remove.each do |item|
-          outlist[item.position - lo][0, 1] = encode(block.op) # - or !
+          outlist[item.position - lo][0, 1] = block.op.encode(@preferred_data_encoding) # - or !
         end
 
         if last && block == last_block && old_missing_newline
-          outlist << encode('\\ No newline at end of file')
+          outlist << @missing_newline
         end
       end
 
-      s << outlist.join(encode("\n")) << encode("\n")
+      s << outlist.join(@newline) << @newline
     end
 
-    s << encode("--- #{r} ----\n")
+    s << "--- #{r} ----\n".encode(@preferred_data_encoding)
     lo, hi = @start_new, @end_new
     inserts = @blocks.reject { |e| e.insert.empty? }
 
     unless inserts.empty?
-      # standard:disable Performance/UnfreezeString
-      outlist = @data_new[lo..hi].map { |e| String.new("#{encode("  ")}#{e.chomp}") }
-      # standard:enable Performance/UnfreezeString
+      outlist = @data_new[lo..hi].map { |e| "#{spaces}#{e.chomp}" }
 
       last_block = inserts[-1]
 
       inserts.each do |block|
         block.insert.each do |item|
-          outlist[item.position - lo][0, 1] = encode(block.op) # + or !
+          outlist[item.position - lo][0, 1] = block.op.encode(@preferred_data_encoding) # + or !
         end
 
         if last && block == last_block && new_missing_newline
-          outlist << encode('\\ No newline at end of file')
+          outlist << @missing_newline
         end
       end
-      s << outlist.join(encode("\n"))
+      s << outlist.join(@newline)
     end
 
     s
   end
-  private :context_diff
 
-  # Generate a range of item numbers to print. Only print 1 number if the
-  # range has only one item in it. Otherwise, it's 'start,end'
+  # Generate a range of item numbers to print. Only print 1 number if the range has only
+  # one item in it. Otherwise, it's 'start,end'
   def context_range(mode, op)
     case mode
     when :old
@@ -288,47 +284,33 @@ class Diff::LCS::Hunk
 
     (s < e) ? "#{s}#{op}#{e}" : e.to_s
   end
-  private :context_range
 
-  # Generate a range of item numbers to print for unified diff. Print number
-  # where block starts, followed by number of lines in the block
-  # (don't print number of lines if it's 1)
+  # Generate a range of item numbers to print for unified diff. Print number where block
+  # starts, followed by number of lines in the block (don't print number of lines if it's
+  # 1)
   def unified_range(mode)
-    case mode
-    when :old
-      return "0,0" if @old_empty
-      s, e = (@start_old + 1), (@end_old + 1)
-    when :new
-      return "0,0" if @new_empty
-      s, e = (@start_new + 1), (@end_new + 1)
-    end
+    s, e =
+      case mode
+      when :old
+        return "0,0" if @old_empty
+        [(@start_old + 1), (@end_old + 1)]
+      when :new
+        return "0,0" if @new_empty
+        [(@start_new + 1), (@end_new + 1)]
+      end
 
     length = e - s + 1
 
     (length <= 1) ? e.to_s : "#{s},#{length}"
   end
-  private :unified_range
 
   def missing_last_newline?(data)
-    newline = encode("\n")
-
     if data[-2]
-      data[-2].end_with?(newline) && !data[-1].end_with?(newline)
+      data[-2].end_with?(@newline) && !data[-1].end_with?(@newline)
     elsif data[-1]
-      !data[-1].end_with?(newline)
+      !data[-1].end_with?(@newline)
     else
       true
     end
   end
-
-  def encode(literal, target_encoding = @preferred_data_encoding)
-    literal.encode target_encoding
-  end
-
-  def encode_as(string, *args)
-    args.map { |arg| arg.encode(string.encoding) }
-  end
-
-  private :encode
-  private :encode_as
 end
